a3b14cb006b3fce90de679078ddd09c2eef60f24
slau8 <slau8@stuy.edu>
2018-03-01 10:55:33 -0500

    matrix done

--- matrix.py
+++ matrix.py
@@ -16,18 +16,33 @@ def make_scale( x, y, z ):
 
 def make_rotX( theta ):    
     matrix = ident(new_matrix())
-    theta = math.radians(theta)
-    matrix[0][0] = math.cos(theta)
-    matrix[0][1] = math.sin(theta) * -1
-    matrix[1][0] = math.sin(theta)
-    matrix[1][0] = math.cos(theta) * -1
+    matrix[1][1] = cos(theta)
+    matrix[1][2] = sin(theta) * -1
+    matrix[2][1] = sin(theta)
+    matrix[2][2] = cos(theta)
     return matrix
 
 def make_rotY( theta ):
-    pass
+    matrix = ident(new_matrix())
+    matrix[0][0] = cos(theta)
+    matrix[0][2] = sin(theta)
+    matrix[2][0] = sin(theta) * -1
+    matrix[2][2] = cos(theta)
+    return matrix
 
 def make_rotZ( theta ):
-    pass
+    matrix = ident(new_matrix())
+    matrix[0][0] = cos(theta)
+    matrix[0][1] = sin(theta) * -1
+    matrix[1][0] = sin(theta)
+    matrix[1][1] = cos(theta)
+    return matrix
+
+def cos(theta):
+    return math.cos(math.radians(theta))
+
+def sin(theta):
+    return math.sin(math.radians(theta))
 
 def print_matrix( matrix ):
     s = ''
@@ -45,14 +60,8 @@ def ident( matrix ):
             else:
                 matrix[c][r] = 0
 
-def scalar_mult( matrix, s ):
-    for r in range( len( matrix[0] ) ):
-        for c in range( len(matrix) ):
-            matrix[c][r]*= s
-            
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-
     point = 0
     for row in m2:
         #get a copy of the next point
@@ -64,7 +73,6 @@ def matrix_mult( m1, m2 ):
                             m1[2][r] * tmp[2] +
                             m1[3][r] * tmp[3])
         point+= 1
-
 
 def new_matrix(rows = 4, cols = 4):
     m = []
